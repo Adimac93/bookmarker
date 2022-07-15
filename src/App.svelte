c1fc08b17e982bcbe129a37f1a2078c7a0321e4e
Adimac93 <Adimac93@gmail.com>
2022-07-15 17:16:18 +0200

    use svelte project template

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,29 @@
 <script lang="ts">
-	export let name: string;
+    import { onMount } from "svelte";
+
+    let count = 0;
+    onMount(() => {
+        const interval = setInterval(() => count++, 1000);
+        return () => clearInterval(interval);
+    });
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<h1>Svelte Template</h1>
+<p>Edit <code>src/routes/index.svelte</code> to trigger HMR</p>
+<p>Counter: <code>{count}</code></p>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+<style lang="sass">
+:global(body)
+	margin: 100px 0px
+	font-family: Arial, Helvetica, sans-serif
+	font-size: 18px
+	background-color: #111318
+	color: #f2f2f2
+	text-align: center
+h1
+	color: #ff3700
+code
+	background: #282b33
+	padding: 2px 5px
+	border-radius: 4px
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,6 @@ import App from "./App.svelte";
 
 const app = new App({
     target: document.body,
-    props: {
-        name: "world",
-    },
 });
 
 export default app;
-invoke("greet", { name: "World" }).then((response) => console.log(response));
